7e90f1776548a14d1cd16014eed0af06e7a878a6
BH <baptistamhernani@gmail.com>
2026-01-30 10:15:59 -0100

    ci: add zod for check if the enviroment add to the project

---
+++ server/src/config/config.ts
@@ -0,0 +1,32 @@
+import dotenv from "dotenv";
+
+import { join } from "path";
+
+import { object, string } from "zod";
+
+const nodeEnv = process.env.NODE_ENV;
+
+dotenv.config({
+	path: join(process.cwd(), "envs", `.env.${nodeEnv}`),
+});
+
+dotenv.config({
+	path: join(process.cwd(), "envs", ".env"),
+});
+
+const CONFIG_API_SCHEMA = object({
+	API_PORT: string().nonempty("API Port is required"),
+});
+
+const CONFIG_PRISMA_SCHEMA = object({
+	DATABASE_URL: string().nonempty("Database URL is required"),
+});
+
+export const Config = {
+	prisma: CONFIG_PRISMA_SCHEMA.parse({
+		DATABASE_URL: process.env.DATABASE_URL,
+	}),
+	api: CONFIG_API_SCHEMA.parse({
+		API_PORT: process.env.API_PORT,
+	}),
+};
